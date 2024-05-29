d857b1464b818ecc47fffa3996bc7e22bac5ce55
brianpocasangre <brianpocasangre1@gmail.com>
2024-05-29 10:33:37 -0700

    install body-parser, create form, and route /submit post method along with parsing the req.body

--- server.js
+++ server.js
@@ -1,11 +1,21 @@
 import express from 'express';
+import bodyParser from 'body-parser';
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
+const __dirName = dirname(fileURLToPath(import.meta.url));
+
 const app = express();
 const port = 3000;
 
+app.use(bodyParser.urlencoded({ extended: true }));
+
 app.get('/', (req, res) => {
-  res.send(
-    '<h1>First Server</h1><p>Welcomes to my first Node.js and Express.js Server.</p>'
-  );
+  res.sendFile(__dirName + '/index.html');
+});
+
+app.post('/submit', (req, res) => {
+  console.log(req.body);
+  // res.send(`<h1> ${JSON.stringify(req.body)} </h1>`);
 });
 
 app.get('/about', (req, res) => {
